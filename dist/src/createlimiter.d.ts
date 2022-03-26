22971b01c27535b768422d017b1c185e97af5a8e
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-26 16:03:55 +0800

    update .gitignore

--- dist/src/createlimiter.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type 空闲状态 = "free" | "full";
-import { AsyncCurrentLimiter } from "./AsyncCurrentLimiter";
-export declare function createlimiter(max: number): AsyncCurrentLimiter;
--- dist/src/promisedefer.d.ts
+++
@@ -1,5 +0,0 @@
-export default function promisedefer(): {
-    promise: Promise<unknown>;
-    reject: (reason?: any) => void;
-    resolve: (value?: any) => void;
-};
